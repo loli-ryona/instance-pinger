57b1c6a0e54caccd3ccb1959251d2bab9a983268
Tristan <43931769+loli-ryona@users.noreply.github.com>
2022-05-22 22:21:15 +1000

    fixed go routine and added smtp email

--- go.mod
+++ go.mod
@@ -2,11 +2,15 @@ module instance-pinger
 
 go 1.18
 
-require github.com/go-ping/ping v1.0.0
+require (
+	github.com/go-ping/ping v1.0.0
+	github.com/xhit/go-simple-mail/v2 v2.11.0
+)
 
 require (
+	github.com/go-test/deep v1.0.8 // indirect
+	github.com/stretchr/testify v1.7.1 // indirect
 	github.com/toorop/go-dkim v0.0.0-20201103131630-e1cd1a0a5208 // indirect
-	github.com/xhit/go-simple-mail/v2 v2.11.0 // indirect
-	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
-	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 )
--- main.go
+++ main.go
@@ -16,33 +16,37 @@ import (
 
 //Vars
 var (
-	inst fwk.Instances
 	smtp fwk.Mail
+	inst fwk.Instances
 )
 
-//Load instances.json
+//Load instances
 func init() {
-	instances, err := os.Open("instances.json")
+	instances, err := os.Open("json/instances.json")
 	if err != nil {
 		fmt.Println("Error loading instances file. Error: ", err)
 		os.Exit(1)
 	}
+	fmt.Println("Successfully opened instances.json")
 
 	if err = js.NewDecoder(instances).Decode(&inst); err != nil {
 		fmt.Println("Error decoding instances. Error: ", err)
 		os.Exit(1)
 	}
+	fmt.Println("Successfully decoded instances")
+	fmt.Println("Initial load done")
 }
 
 //Main script
 func main() {
+	fmt.Println("Starting main script")
+
 	//Create Array for downed instances
 	failedPings := 0
-	instLength := []fwk.Instances{}
-	downedInst := []string{
-		"<h1>Instance Alert</h1>",
-		"<p>The following instances have not<br>returned a ping in the last 5 minutes<br></p>",
-	}
+	instLength := inst.Name
+	fmt.Println(instLength)
+	downedInst := []string{}
+	fmt.Println(downedInst)
 
 	//Wait Group shit
 	var wg sync.WaitGroup
@@ -52,30 +56,36 @@ func main() {
 	//Go Routine (im shit at these)
 	fmt.Println("Starting pings")
 	for i := 0; i < len(instLength); i++ {
-		go func(i int) {
+		cum := i
+		go func(cum int) {
 			//Pinging Instance
-			fmt.Println("Pinging " + inst.Name[i])
-			online, err := pingInstance(inst.Addr[i])
+			defer wg.Done()
+			time.Sleep(time.Second)
+			fmt.Println("Pinging " + inst.Name[cum])
+			online, err := pingInstance(inst.Addr[cum])
 			if err != nil {
 				fmt.Println(err.Error())
 			}
 			if online {
-				fmt.Println("Instance " + inst.Name[i] + " returned status of: UP")
+				fmt.Println("Instance " + inst.Name[cum] + " returned status of: UP")
 			} else {
 				failedPings++
-				downedInst = append(downedInst, inst.Name[i]+" - "+inst.Addr[i])
-				fmt.Printf("instance" + inst.Name[i] + " returned status of: DOWN")
+				downedInst = append(downedInst, "<p>"+inst.Name[cum]+" - "+inst.Addr[cum]+"</p><br>\n")
+				fmt.Printf("Instance " + inst.Name[cum] + " returned status of: DOWN\n")
 			}
-		}(i)
-	}
 
+		}(cum)
+	}
 	wg.Wait()
 
+	fmt.Println(failedPings)
 	fmt.Println("Finished pings")
 
 	if failedPings >= 1 {
-		fmt.Printf("Found %v downed instances, preparing alert mail.", failedPings)
-		sendAlert()
+		fmt.Printf("Found %v downed instances, preparing alert mail.\n", failedPings)
+		sendAlert(downedInst)
+	} else {
+		fmt.Println("Found 0 downed instances")
 	}
 }
 
@@ -90,6 +100,7 @@ func pingInstance(addr string) (bool, error) {
 		fmt.Println(err)
 		return false, err
 	}
+	pinger.Timeout = time.Second * 10
 
 	//Send and receive 4 bytes
 	pinger.Count = 4
@@ -111,48 +122,74 @@ func pingInstance(addr string) (bool, error) {
 
 //Send alert email if there is downed instances
 //This uses xhit's go-simple-mail library
-func sendAlert() {
+func sendAlert(downed []string) {
 	//Vars
 	encryptionType := mail.EncryptionNone
 
-	//Read mailBody.html
-	body, err := os.ReadFile("mailBody.txt")
-	if err != nil {
-		fmt.Println("Error reading mailBody.txt. Error: ", err)
-		os.Exit(1)
+	//Build body structure
+	bodyStr := `<h1>Instance Alert</h1><br>
+<p>The following instances have not<br>returned a ping in the last 5 minutes</p><br>` + "\n"
+	fmt.Println("This is the current body structure")
+	fmt.Println(bodyStr)
+	fmt.Println("Adding downed instances to structure")
+	for i := 0; i < len(downed); i++ {
+		bodyStr = bodyStr + downed[i]
 	}
+	fmt.Println("This the body structure after added downed instances")
+	fmt.Println(bodyStr)
 
-	bodyStr := string(body)
+	//This is just for debugging html body errors
+	//Ive noticed gmail duplicates the bodies downed instances
+	/* os.Create("body.html")
+	f, err := os.OpenFile("body.html", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error oppening body.html. Error: ", err)
+	}
+	if _, err := f.WriteString(bodyStr); err != nil {
+		fmt.Println("Error writing to body.html. Error: ", err)
+	}
+	if err := f.Close(); err != nil {
+		fmt.Println("Error closing body.html. Error: ", err)
+	} */
 
 	//Start mail client
 	server := mail.NewSMTPClient()
+	fmt.Println("Starting mail client")
 
 	//Load SMTP settings
-	mailSettings, err := os.Open("smtp.json")
+	mailSettings, err := os.Open("json/smtp.json")
 	if err != nil {
 		fmt.Println("Error loading smtp file. Error: ", err)
 		os.Exit(1)
 	}
+	fmt.Println("Successfully loaded smtp.json")
 
 	if err = js.NewDecoder(mailSettings).Decode(&smtp); err != nil {
 		fmt.Println("Error decoding smtp settings. Error: ", err)
 		os.Exit(1)
 	}
+	fmt.Println("Successfully loaded SMTP settings")
 
 	switch {
 	case smtp.Encryption == "None":
 		encryptionType = mail.EncryptionNone
+		fmt.Println("Detected Encryption Type: None")
 	case smtp.Encryption == "SSL":
 		encryptionType = mail.EncryptionSSL
+		fmt.Println("Detected Encryption Type: SSL")
 	case smtp.Encryption == "SSLTLS":
 		encryptionType = mail.EncryptionSSLTLS
+		fmt.Println("Detected Encryption Type: SSL/TLS")
 	case smtp.Encryption == "STARTTLS":
 		encryptionType = mail.EncryptionSTARTTLS
+		fmt.Println("Detected Encryption Type: STARTTLS")
 	case smtp.Encryption == "TLS":
 		encryptionType = mail.EncryptionTLS
+		fmt.Println("Detected Encryption Type: TLS")
 	}
 
 	//Assign SMTP settings
+	fmt.Println("Pre-connection SMTP configuring")
 	server.Host = smtp.Host
 	server.Port = smtp.Port
 	server.Username = smtp.Username
@@ -162,20 +199,27 @@ func sendAlert() {
 	server.ConnectTimeout = 10 * time.Second
 	server.SendTimeout = 10 * time.Second
 	server.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	fmt.Println("Pre-connection SMTP settings configured")
 
 	//Connect to client
+	fmt.Println("Connecting to client")
 	smtpClient, err := server.Connect()
 
 	if err != nil {
 		fmt.Println("Expected nil, instead returned: ", err)
 	}
+	fmt.Println("Client Connected")
 
 	//Creating actual message
+	fmt.Println("Building Message")
 	email := mail.NewMSG()
 	email.SetFrom(smtp.From).AddTo(smtp.To).SetSubject(smtp.Subject)
+	for i := 0; i < len(downed); i++ {
+		bodyStr = bodyStr + downed[i]
+	}
 	email.SetBody(mail.TextHTML, bodyStr)
-	email.AddAlternative(mail.TextPlain, smtp.Alternative)
-	email.SetDate(time.Now().String())
+	//email.AddAlternative(mail.TextPlain, smtp.Alternative)
+	//email.SetDate(time.Now().String())
 	email.SetPriority(mail.PriorityHigh)
 
 	if email.Error != nil {
@@ -183,9 +227,14 @@ func sendAlert() {
 	}
 
 	//Send email
+	fmt.Println("Sending email")
 	err = email.Send(smtpClient)
 	email.GetError()
 	if err != nil {
 		fmt.Println("Expected nil, instead returned: ", err)
+		fmt.Println("Email failed to send")
+		os.Exit(1)
 	}
+	fmt.Println("Email sent successfully!")
+	os.Exit(0)
 }
